c75e908192ba8c2907c3436150a4b5f993ef9342
Alex Horning <67214695+makeitouthill@users.noreply.github.com>
2023-02-09 08:01:01 -0800

    Added a main.handlebars to be displayed when you visit Url

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const sequelize = require('./config/connection');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log('Server listening on: http://localhost:' + PORT);
-});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/homeRoutes'));
+
+
+app.use(routes);
+
+// sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {console.log('Server listening on: http://localhost:' + PORT);
+    })
+// });
   
